c208a6d4f8815bb575628b17e37a49b259b8e3ad
TIMOTHY KIM <168173100+timothy2077@users.noreply.github.com>
2024-12-19 21:06:35 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,13 +40,23 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 
 
-# New section to display smoothiefroot nutrition information
+
 import requests
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+if ingredients_list:
+    ingredients_string = ''
 
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get(
+            "https://my.smoothiefroot.com/api/fruit/watermelon"
+        )
+        sf_df = st.dataframe(
+            data=smoothiefroot_response.json(), 
+            use_container_width=True
+        )
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
 
